bf71876a41163d1837f7963d7803c3aed78092d3
Jaehwan Lee <chrisjae508@gmail.com>
2024-05-31 16:10:52 +0900

    Modify the directory of gpt2 dataset to /data

--- dataset/download_vocab.sh
+++ dataset/download_vocab.sh
@@ -1,2 +1,2 @@
-wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
-wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json -O data/gpt2-vocab.json
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt -O data/gpt2-merges.txt
